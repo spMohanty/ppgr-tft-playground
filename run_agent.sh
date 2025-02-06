17d75b6ecb8c5c3b7458effe522cf93626c91a74
Sharada Prasanna Mohanty <spmohanty91@gmail.com>
2025-02-06 08:57:06 +0100

    add dataset cache

--- run_agent.sh
+++ run_agent.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
+#SBATCH --job-name=ppgr_tft_sweep-food-only
+#SBATCH --gpus=1
+#SBATCH --mem-per-gpu=32G
+#SBATCH --cpus-per-gpu=8
+#SBATCH --partition=l40s
+#SBATCH --time=12:00:00
+#SBATCH --output=logs/slurm/%j.out
+#SBATCH --error=logs/slurm/%j.err
 
-# micromamba activate ppgr
+# Configuration variables
+WANDB_ENTITY="mfr-ppgr-2025"
+WANDB_PROJECT="tft-ppgr-2025"
+WANDB_SWEEP_ID="geysk990"
 
-wandb agent 5jaoynd4 -p pytorch-forecasting-playground -e mfr-ppgr-2025
+NUM_AGENTS=3 # 6 for h100 and 3 for l40s
+
+cd /home/mohanty/food/pytorch-forecasting-playground
+
+# Activate environment (uncomment if needed)
+source ~/.bashrc
+micromamba activate ppgr
+
+for i in $(seq 1 $NUM_AGENTS); do
+    CUDA_VISIBLE_DEVICES=0 wandb agent $WANDB_SWEEP_ID -p $WANDB_PROJECT -e $WANDB_ENTITY &
+done
+
+wait
+
 
